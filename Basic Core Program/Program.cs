cf50b4cbf9aa8942f1537929061ca9c9321dd3c6
Akshata72 <sableakshata880@gmail.com>
2022-03-14 16:20:23 +0530

    Added:UC1-FlipCoin

---
+++ Basic Core Program/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace BasicCoreProgram
+{
+
+    class BasicCoreProgram
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Choose 1 for FlipCoin");
+           
+            int Option = int.Parse(Console.ReadLine());
+
+            switch(Option)
+            {
+                case 1:
+                    FlipCoin flipCoin = new FlipCoin();
+                    flipCoin.FlipCoins();
+                    break;
+                
+
+
+
+
+
+            }
+
+
+
+        }
+
+    }
+}
